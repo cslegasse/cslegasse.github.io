cc95178566608e3d16e6926bb465f5d8fc1b9e20
cslegasse <legasseahs1@gmail.com>
2025-02-12 09:29:04 -0500

    pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+;
+
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/cslegasse.github.io/' : '',
+  basePath: isProd ? '/cslegasse.github.io' : '',
+  output: 'export'
 };
 
-module.exports = nextConfig;
+export default nextConfig;

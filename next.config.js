affe3408c6e0ad212b9c726d6ce54d567b5d32b5
cslegasse <legasseahs1@gmail.com>
2025-02-20 06:11:26 -0500

    stupid config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,21 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   output: "export", 
-//   reactStrictMode: true,
-//   basePath: "/cslegassse.github.io",
-// };
 
-// module.exports = nextConfig;
+// deploy
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export", 
   reactStrictMode: true,
+  basePath: "/cslegassse.github.io",
 };
 
 module.exports = nextConfig;
+
+
+// local
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
+
+// module.exports = nextConfig;

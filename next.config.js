f311bbf1419a0d98dcbad1c839c270c3bb511d32
cslegasse <legasseahs1@gmail.com>
2025-03-07 04:32:15 -0500

    config bs again

--- next.config.js
+++ next.config.js
@@ -1,20 +1,20 @@
 
 
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   output: "export", 
-//   reactStrictMode: true,
-//   basePath: "/cslegassse.github.io",
-// };
-
-// module.exports = nextConfig;
-
-
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export", 
   reactStrictMode: true,
+  basePath: "/cslegassse.github.io",
 };
 
 module.exports = nextConfig;
+
+
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
+
+// module.exports = nextConfig;

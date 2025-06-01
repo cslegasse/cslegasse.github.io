75b8620a779cc1520b44d74fa110b5719201d8af
cslegasse <legasseahs1@gmail.com>
2025-06-02 00:21:19 +0900

    res del 2

--- next.config.js
+++ next.config.js
@@ -1,19 +1,19 @@
 
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   output: "export", 
-//   reactStrictMode: true,
-//   basePath: "/cslegassse.github.io",
-// };
-
-// module.exports = nextConfig;
-
-
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export", 
   reactStrictMode: true,
+  basePath: "/cslegassse.github.io",
 };
 
 module.exports = nextConfig;
+
+
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
+
+// module.exports = nextConfig;

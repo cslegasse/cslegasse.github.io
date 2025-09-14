9000dbfb5959bd88eefc225af93b655fdf64f801
cslegasse <legasseahs1@gmail.com>
2025-09-14 03:43:14 -0400

    direct flow

--- next.config.js
+++ next.config.js
@@ -1,19 +1,19 @@
 
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   output: "export", 
-//   reactStrictMode: true,
-//   basePath: "/cslegassse.github.io",
-// };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export", 
+  reactStrictMode: true,
+  basePath: "/cslegassse.github.io",
+};
 
-// module.exports = nextConfig;
+module.exports = nextConfig;
 
 
 // local
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
 
-module.exports = nextConfig;
+// module.exports = nextConfig;

f554379311f8d169cf7ba4053e538bf7b8d7270f
cslegasse <legasseahs1@gmail.com>
2025-12-19 00:25:33 -0500

    slugged npm

--- next.config.js
+++ next.config.js
@@ -1,24 +1,11 @@
 
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   output: "export", 
-//   reactStrictMode: true,
-//   basePath: "/cslegasse.github.io",
-// };
-
-// module.exports = {
-//   experimental: {
-//     appDir: true
-//   }
-// }
-
-// module.exports = nextConfig;
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export", 
   reactStrictMode: true,
+  basePath: "/cslegasse.github.io",
 };
+
 module.exports = {
   experimental: {
     appDir: true
@@ -26,3 +13,16 @@ module.exports = {
 }
 
 module.exports = nextConfig;
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
+// module.exports = {
+//   experimental: {
+//     appDir: true
+//   }
+// }
+
+// module.exports = nextConfig;
